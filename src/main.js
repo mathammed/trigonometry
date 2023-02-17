438dcde9236221494432fd58bde0a4d76ac11eb0
3Dlyra <3dlyra.com@gmail.com>
2023-02-17 23:51:39 +0500

    Degree based coordinates

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ const { scene } = App.setup()
 
 const vertices = []
 
+const degree = Math.PI / 180
+
+const max = 45 // 45°
+
 for ( let d = 1; d <= 45; d++ ) {
 
-	vertices.push( Math.cos( d ), Math.sin( d ), 0 )
+	vertices.push( Math.cos( d * degree ), Math.sin( d * degree ), 0 )
 }
 
 scene.add( App.drawPoints( vertices, 0xffffff ) )
