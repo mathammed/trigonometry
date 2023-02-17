bf4ad1dba92bd6f1387fce8fb682345f3ab7aaf5
3Dlyra <3dlyra.com@gmail.com>
2023-02-17 22:18:15 +0500

    Artificial curve

--- src/main.js
+++ src/main.js
@@ -4,9 +4,26 @@ const { scene } = App.setup()
 
 const vertices = []
 
-for ( let x = -2; x <= 2; x += 0.5 ) {
+for ( let x = -2; x <= 2; x += 1 ) {
 
-	vertices.push( x, 0, 0 ) // x, y, z
+	if ( x < 0 ) {
+
+		// LEFT from ORIGIN
+
+		vertices.push( x, -1, 0 ) // x, y, z
+	}
+	else if ( x > 0 ) {
+
+		// RIGHT from ORIGIN
+
+		vertices.push( x, 1, 0 ) // x, y, z
+	}
+	else {
+
+		// CENTER
+
+		vertices.push( 0, 0, 0 ) // x, y, z
+	}
 }
 
 scene.add( App.drawPoints( vertices, 0xffffff ) )
